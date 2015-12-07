e429c75f55cfd14cf86d0fa870ff65d5e5328d21
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-07 20:47:59 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+var _ = require('lodash'),
+    google = require('googleapis'),
+    OAuth2 = google.auth.OAuth2;
+
+var pickData = ['id', 'title', 'defaultOpenWithLink'];
+var mimeType = 'application/vnd.google-apps.spreadsheet';
+
 module.exports = {
+    checkAuthOptions: function (step, dexter) {
+
+        if(!step.input('name').first()) {
+
+            this.fail('A [name] input variable is required for this module');
+        }
+
+        if(!dexter.environment('google_access_token')) {
+
+            this.fail('A google_access_token environment variable is required for this module');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +26,24 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        this.checkAuthOptions(step, dexter);
+
+        var oauth2Client = new OAuth2();
+        var service = google.drive('v2');
+
+        oauth2Client.setCredentials({access_token: dexter.environment('google_access_token'), refresh_token: dexter.environment('google_refresh_token')});
+        google.options({ auth: oauth2Client });
+
+        service.files.insert({
+            convert: true,
+            resource: {
+                    title: step.input('name').first(),
+                    mimeType:mimeType
+                }
+        }, function (err, data) {
+
+            err? this.fail(err) : this.complete(_.pick(data, pickData));
+        }.bind(this));
     }
 };
